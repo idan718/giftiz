a175bb9a40ed244828ba701a08eff82d4796d496
idan718 <idanisim.n1@gmail.com>
2025-02-27 20:35:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 
+const button = document.getElementById("myButton")
 
-console.log("server online")
-const name = document.getElementById("inputName").value
-const saveButton = document.getElementById("saveButton")
-saveButton.addEventListener("click", function(){
-    let name = document.getElementById("inputName").value
-    let showName = document.getElementById("username")
-    showName.innerHTML = "תודה " + name + "על שנרשמת"
-    saveButton.style.visibility = "hidden"
-    name.style.visibility = "hidden"
+button.addEventListener("click", function() {
 
-    if (name =="") {
-        showName.innerHTML = "שם לא תקין"
-        saveButton.style.visibility = "visible"
-name.style.visibility = "visible"
-    }
+    alert("button clicked")
+})
+
+const box = document.getElementById("box")
+box.addEventListener("mouseover", function() {
+box.style.background = "red"
+    
+})
+
+box.addEventListener("mouseout", function(){
+box.style.background = "blue"
+
+})
+
+const inputField = document.getElementById("inputField")
+inputField.addEventListener("keydown", function(event){
+    console.log("key pressed", event.key)
+})
+
+button.addEventListener("click", function() {
+let name = document.getElementById("inputField").value
+localStorage.setItem("name", name)
+let savedname =  localStorage.getItem("name")
+console.log(savedname)
+})
+
+const helloButton = document.getElementById("helloButton")
+helloButton.addEventListener("click", function() {
+console.log("hello world")
+alert("HI")
+
 })
